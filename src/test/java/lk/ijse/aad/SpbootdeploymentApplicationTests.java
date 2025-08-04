953d40825785a0a9fbac9f1358355011a175a9a1
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-04 10:03:24 +0530

    first commit

---
+++ src/test/java/lk/ijse/aad/SpbootdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
