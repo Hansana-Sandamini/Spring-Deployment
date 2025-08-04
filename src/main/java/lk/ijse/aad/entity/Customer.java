72cac756c7a42d8a143c6da6e8038f25e015fe39
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-04 11:42:47 +0530

    content updated

--- src/main/java/lk/ijse/aad/entity/Customer.java
+++ src/main/java/lk/ijse/aad/entity/Customer.java
@@ -14,9 +14,7 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
     private String email;
 }
